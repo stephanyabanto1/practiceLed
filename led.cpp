012ee9adca839b4e0843c5de36f4fe909c05ac45
Stephany Abanto <63083633+stephanyabanto1@users.noreply.github.com>
2023-08-16 19:55:46 -0400

    Create led.cpp

---
+++ led.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <wiringPi.h>
+using namespace std;
+
+
+
+int main(){
+
+    wiringPiSetup();
+
+    pinMode(0, OUTPUT);
+    for (;;)
+  {
+    digitalWrite (0, HIGH) ; delay (500) ;
+    digitalWrite (0,  LOW) ; delay (500) ;
+  }
+  return 0 ;
+}
+
+
+
+
+
