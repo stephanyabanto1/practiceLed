085d56b98aa7f2715ccdfa48b7d14a7b878838b2
Stephany Abanto <63083633+stephanyabanto1@users.noreply.github.com>
2023-08-24 18:35:02 -0400

    Update led.py

--- led.py
+++ led.py
@@ -19,3 +19,5 @@ lcd_rows = 2
 lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
 
 lcd.message('Berries n \n Cream')
+
+set_cursor(1,5)
