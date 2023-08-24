b7d899e00fd09a380711469458e3a7b5bd324e48
Stephany Abanto <63083633+stephanyabanto1@users.noreply.github.com>
2023-08-24 18:58:37 -0400

    Update led.py

--- led.py
+++ led.py
@@ -18,10 +18,18 @@ lcd_rows = 2
 
 lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
 
-lcd.set_cursor(5,0)
-lcd.message('how are you')
+lcd.set_cursor(0,0)
 
-time.sleep(5)
+ queen = lcd.message('hi')
+
+time.sleep(2)
+
+lcd.set_cursor(3,0)
+lcd.message(2)
+clear(queen)
+
+
+
 
 
 
