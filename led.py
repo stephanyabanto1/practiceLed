7d3b93ff840eecd1106e20d1d51699e075e2a6eb
Stephany Abanto <63083633+stephanyabanto1@users.noreply.github.com>
2023-08-24 17:42:31 -0400

    Update led.py

--- led.py
+++ led.py
@@ -19,16 +19,3 @@ lcd_rows = 2
 lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
 
 lcd.message('Hello\nworld!')
-# Wait 5 seconds
-
-time.sleep(5.0)
-lcd.clear()
-
-
-# Wait 5 seconds
-time.sleep(5.0)
-lcd.clear()
-lcd.message('Goodbye\nWorld!')
-
-time.sleep(5.0)
-lcd.clear()
