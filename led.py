23b43a39db6b926160992413dbaf0f086749baa9
Stephany Abanto <63083633+stephanyabanto1@users.noreply.github.com>
2023-08-24 17:04:57 -0400

    Update led.py

--- led.py
+++ led.py
@@ -13,7 +13,7 @@ dB7 = 22
 
 #initialize the pins with the 
 
-lcd = LCD.Adafruit_CharLCD(rs_pin, ena_pin, dB4. dB5, dB6, dB7 )
+lcd = LCD.Adafruit_CharLCD(rs_pin, ena_pin, dB4, dB5, dB6, dB7 )
 
 lcd.message('Hello World')
 
