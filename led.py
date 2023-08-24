636c76fade82abf8cf279e9441a996545edf1f32
Stephany Abanto <63083633+stephanyabanto1@users.noreply.github.com>
2023-08-24 18:41:36 -0400

    Update led.py

--- led.py
+++ led.py
@@ -13,8 +13,8 @@ lcd_d7 = 22
 lcd_backlight = 2
 
 # Define LCD column and row size for 16x2 LCD.
-lcd_columns = 16
-lcd_rows = 2
+lcd_columns = 20
+lcd_rows = 3
 
 lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
 
