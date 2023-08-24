13fe653c697a5a2b3458dd18d12f769cac5090c9
Stephany Abanto <63083633+stephanyabanto1@users.noreply.github.com>
2023-08-24 17:34:35 -0400

    Update led.py

--- led.py
+++ led.py
@@ -1,26 +1,38 @@
 import time
 import Adafruit_CharLCD as LCD
 
+#!/usr/bin/python
+# Example using a character LCD connected to a Raspberry Pi
+import time
+import Adafruit_CharLCD as LCD
 
-#rasberry pi pin set up 
+# Raspberry Pi pin setup
+lcd_rs = 25
+lcd_en = 24
+lcd_d4 = 23
+lcd_d5 = 17
+lcd_d6 = 18
+lcd_d7 = 22
+lcd_backlight = 2
 
-rs_pin = 25
-ena_pin = 24
-dB4 = 23
-dB5 = 17
-dB6 = 18
-dB7 = 22
-lcd_colums = 16
+# Define LCD column and row size for 16x2 LCD.
+lcd_columns = 16
 lcd_rows = 2
-lcd_backlight =2
 
-#initialize the pins with the 
+lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
 
-lcd = LCD.Adafruit_CharLCD(rs_pin, ena_pin, dB4, dB5, dB6, dB7, lcd_colums, lcd_rows, lcd_backlight)
+lcd.message('Hello\nworld!')
+# Wait 5 seconds
 
-lcd.message('Hello World')
+time.sleep(5.0)
+lcd.clear()
+text = raw_input("Type Something to be displayed: ")
+lcd.message(text)
 
+# Wait 5 seconds
+time.sleep(5.0)
+lcd.clear()
+lcd.message('Goodbye\nWorld!')
 
-
-
-
+time.sleep(5.0)
+lcd.clear()
