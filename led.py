49fe8122790d5889e6c2499e8c26c383fb36ccbf
Stephany Abanto <63083633+stephanyabanto1@users.noreply.github.com>
2023-08-24 19:28:33 -0400

    Update led.py

--- led.py
+++ led.py
@@ -18,7 +18,7 @@ lcd_rows = 2
 
 lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
 
-for i in range(lcd_columns)
+for i in range(lcd_columns):
     lcd.message(i)
 
 
