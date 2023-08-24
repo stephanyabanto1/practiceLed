3dc88a71bed854988883f6a7a4c4552cf6e3bcc9
Stephany Abanto <63083633+stephanyabanto1@users.noreply.github.com>
2023-08-24 19:12:35 -0400

    Update led.py

--- led.py
+++ led.py
@@ -18,11 +18,13 @@ lcd_rows = 2
 
 lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
 
-lcd.set_cursor(0,0)
+lcd.set_cursor(1,4)
 
 
 
 lcd.message('To tell you the truth ')
+
+time.sleep(3)
 
 lcd.clear()
 
