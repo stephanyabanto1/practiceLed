50ebd1944cf34fe17549a9bb9407a5c209cc3afa
Stephany Abanto <63083633+stephanyabanto1@users.noreply.github.com>
2023-08-24 15:24:45 -0400

    Update led.py

--- led.py
+++ led.py
@@ -1,6 +1,8 @@
 import time
 import Adafruit_CharLCD as LCD
 
+GPIO.setwarnings(False)
+
 #rasberry pi pin set up 
 
 rs_pin = 25
@@ -15,4 +17,6 @@ dB7 = 22
 lcd = LCD.Adafruit_CharLCD(rs_pin, ena_pin, dB4. dB5, dB6, dB7 )
 
 lcd.message('Hello World')
+
+
 
