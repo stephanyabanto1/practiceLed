8ba038e02fb579e5f8ae65f045f4db8c10e36a52
Stephany Abanto <63083633+stephanyabanto1@users.noreply.github.com>
2023-08-24 20:22:49 -0400

    Update led.py

--- led.py
+++ led.py
@@ -19,22 +19,22 @@ lcd_rows = 2
 lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
 
 while(True):
-    for i in range(17):
+    # for i in range(17):
         # num = str(i)
         # lcd.clear()
         # lcd.set_cursor(-1,0)
         # lcd.set_cursor(i-1,0)
-        lcd.set_cursor(-1,0)
-        lcd.message('HI')
+        # lcd.set_cursor(-1,0)
+        # lcd.message('HI')
         # lcd.set_cursor(i-17,0)
         # lcd.message('HI')
 
         # time.sleep(0.2)
         
-    # lcd.clear()
-    # lcd.set_left_to_right()
-    # lcd.message('Hello')
-    # time.sleep(0.2)
+    lcd.clear()
+    lcd.message('Hello')
+    lcd.set_left_to_right()
+    time.sleep(0.2)
 
 
 
