58e82e02833ac86637677509393ff8378a5b7e30
Stephany Abanto <63083633+stephanyabanto1@users.noreply.github.com>
2023-08-24 21:02:09 -0400

    Update led.py

--- led.py
+++ led.py
@@ -19,9 +19,10 @@ lcd_rows = 2
 lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
 
 message = 'Hello'
-lcd.color = [0, 100, 0]
+
 
 while(True):
+    lcd.color = [0, 100, 0]
     # for i in range(17):
         # num = str(i)
         # lcd.clear()
